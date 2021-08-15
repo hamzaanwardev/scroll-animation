4cdf7137ce19126d656af688117eb45b26a5e360
Hamza Anwar <hamzaanwardev@gmail.com>
2021-08-15 23:31:51 +0500

    making javaScript cod

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-const boxes = document.querySelectorAll(".box")
+const boxes = document.querySelectorAll(".box");
+
+window.addEventListener ("scroll", checkboxes)
+
+checkboxes();
+
+function checkboxes() {
+   const triggerBottom = window.innerHeight / 5 * 4;
+
+   boxes.forEach(box => {
+       const boxTop = box.getBoundingClientRect()
+   })
+}
